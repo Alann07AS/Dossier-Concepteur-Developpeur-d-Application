975382e9dff7a9381f465d7303ed1ce3b06449f6
Alann <AlannSchnebelen@hotmail.com>
2024-06-04 11:03:47 +0200

    very first version, with md list and md viewer

---
+++ static/js/list.js
@@ -0,0 +1,32 @@
+const list = document.getElementById("list")
+const md = document.getElementById("md")
+
+// List of HTML files to load (one at a time or based on some condition)
+const htmlFiles = [
+    "Maquetter_une_application",
+    "Développer_une_interface_utilisateur_de_type_desktop",
+    "Développer_des_composants_d'accès_aux_données",
+    "Développer_la_partie_front-end_d'une_interface_utilisateur_web",
+    "Développer_la_partie_back-end_d'une_interface_utilisateur_web",
+    "Concevoir_une_base_de_données",
+    "Mettre_en_place_une_base_de_données",
+    "Développer_des_composants_dans_le_langage_d'une_base_de_données",
+    "Collaborer_à_la_gestion_d'un_projet_informatique_et_à_l'organisation_de_l'environnement_de_développement",
+    "Concevoir_une_application",
+    "Développer_des_composants_métier",
+    "Construire_une_application_organisée_en_couches",
+    "Développer_une_application_mobile",
+    "Préparer_et_exécuter_les_plans_de_tests_d'une_application",
+    "Préparer_et_exécuter_le_déploiement_d'une_application"
+];
+
+htmlFiles.forEach(f=>{
+    const el = document.createElement("div")
+    el.innerText = f.replaceAll("_", " ")
+    el.classList.add("buble")
+    el.classList.add("file")
+    el.onclick = (e)=>{
+        loadHTMLContent(f+".html", md)
+    }
+    list.appendChild(el)
+})
--- static/js/md_loader.js
+++ static/js/md_loader.js
@@ -1,5 +1,5 @@
 // Function to load HTML content into the body
-function loadHTMLContent(file, el) {
+function loadHTMLContent(file, el, anchor=true) {
     if (!file || !el) return
     // Create a new XMLHttpRequest object
     var xhr = new XMLHttpRequest();
@@ -13,32 +13,10 @@ function loadHTMLContent(file, el) {
             // Insert the HTML content into the body
             el.classList.toggle("markdown-body", true)
             el.innerHTML = xhr.responseText;
+            if (anchor) window.location.href = "#"+el.id
         }
     };
 
     // Send the request
     xhr.send();
 }
-
-// List of HTML files to load (one at a time or based on some condition)
-const htmlFiles = [
-    "Maquetter_une_application.html",
-    "Développer_une_interface_utilisateur_de_type_desktop.html",
-    "Développer_des_composants_d'accès_aux_données.html",
-    "Développer_la_partie_front-end_d'une_interface_utilisateur_web.html",
-    "Développer_la_partie_back-end_d'une_interface_utilisateur_web.html",
-    "Concevoir_une_base_de_données.html",
-    "Mettre_en_place_une_base_de_données.html",
-    "Développer_des_composants_dans_le_langage_d'une_base_de_données.html",
-    "Collaborer_à_la_gestion_d'un_projet_informatique_et_à_l'organisation_de_l'environnement_de_développement.html",
-    "Concevoir_une_application.html",
-    "Développer_des_composants_métier.html",
-    "Construire_une_application_organisée_en_couches.html",
-    "Développer_une_application_mobile.html",
-    "Préparer_et_exécuter_les_plans_de_tests_d'une_application.html",
-    "Préparer_et_exécuter_le_déploiement_d'une_application.html"
-];
-
-// htmlFiles.forEach(el => {
-//     loadHTMLContent(el, document.body)
-// });
