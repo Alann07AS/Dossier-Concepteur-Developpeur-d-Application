99ee49a55200f05c509ef305baca959b4baefa4b
Alann <AlannSchnebelen@hotmail.com>
2024-06-07 17:26:11 +0200

    save android

--- static/js/scroll.js
+++ static/js/scroll.js
@@ -1,32 +1,43 @@
-var page = 0;
-let max_page = 1
-const scroll= (condition)=>{
+{
     const html = document.querySelector('html')
     const md = document.querySelector('#md-div')
-    if ((page * window.innerHeight) != html.scrollTop) return
-    if (md.scrollHeight > window.innerHeight && md.scrollTop !== 0) return
-    page += condition ? 1 /**down */ :-1 //up
-    if (page > max_page) page = max_page
-    if (page < 0) page = 0
+    html.scrollTo(0, 0)
 
-    html.scrollTo(0, page * window.innerHeight)
-}
+    var page = 0;
+    let max_page = 1
+    var timeout
+    const scroll = (condition) => {
+        if ((page * window.innerHeight) - html.scrollTop) return
+        if (md.scrollHeight > window.innerHeight && md.scrollTop !== 0) {
+            if (!timeout) timeout = setTimeout(()=>{
+                html.scrollTo(0, (page * window.innerHeight))
+                timeout = undefined
+            }, 1000)//reset fixed
+            return
+        }
+        page += condition ? 1 /**down */ : -1 //up
+        if (page > max_page) page = max_page
+        if (page < 0) page = 0
 
-window.addEventListener("wheel", (e) => {
-    scroll(e.deltaY > 0)
-})
+        html.scrollTo(0, page * window.innerHeight)
+    }
 
-var lastY = 0;
-window.addEventListener("touchstart", (e) => {
-    lastY = e.touches[0].pageY
-})
-window.addEventListener("touchmove", (e) => {
-    const dif = lastY - e.touches[0].pageY
-    if (Math.abs(dif) > 40) scroll(dif > 0)
-    // lastY = e.touches[0].pageY
-})
+    window.addEventListener("wheel", (e) => {
+        scroll(e.deltaY > 0)
+    })
 
-function ShowMD(b = true) {
-    max_page = b ? 2 : 1
-    scroll(b)
+    var lastY = 0;
+    window.addEventListener("touchstart", (e) => {
+        lastY = e.touches[0].pageY
+    })
+    window.addEventListener("touchmove", (e) => {
+        const dif = lastY - e.touches[0].pageY
+        if (Math.abs(dif) > 40) scroll(dif > 0)
+        // lastY = e.touches[0].pageY
+    })
+
+    function ShowMD(b = true) {
+        max_page = b ? 2 : 1
+        scroll(b)
+    }
 }
